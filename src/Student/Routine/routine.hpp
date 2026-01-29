e40ea9214210df08361f86ee9a90122370812b53
KanetahKhan <kanetahkhan@gmail.com>
2026-01-29 12:42:53 +0600

    yyyyyyyyyyyyyyyyyy

--- src/Student/Routine/routine.hpp
+++ src/Student/Routine/routine.hpp
@@ -1,67 +1,54 @@
 #pragma once
-#include "../App/Person.hpp"
-#include <vector>
 #include <string>
+#include <vector>
 #include <ctime>
-
 
 struct ClassSchedule
 {
-    std::string day;          
-    std::string startTime;    
-    std::string endTime;      
-    std::string courseCode;  
-    std::string courseName;   
-    std::string room;         
-    std::string instructor;   
+    std::string day;
+    std::string startTime;
+    std::string endTime;
+    std::string courseCode;
+    std::string courseName;
+    std::string room;
+    std::string instructor;
 };
-
 
 struct ClassReminder
 {
     ClassSchedule classInfo;
     int minutesUntil;
     bool isToday;
-    bool isUpcoming;  
+    bool isUpcoming;
 };
 
-class Student : public Person
+class Routine
 {
-public:
-    Student(std::string u, std::string p, std::string dept, std::string prog, int sem);
-    void showMenu() override;
-
-   
-    std::string getDepartment() const { return department; }
-    std::string getProgram() const { return program; }
-    int getSemester() const { return semester; }
-
 private:
-  
-    std::string department;
-    std::string program;
-    int semester;
-
-    
+    std::string username;
     std::vector<ClassSchedule> weeklySchedule;
     std::vector<ClassReminder> activeReminders;
 
-    
+public:
+    Routine(const std::string& user);
+    ~Routine();
+
     void loadRoutineFromFile();
-    void viewSchedule();
     void viewTodaySchedule();
     void viewWeeklySchedule();
     void editRoutine();
     void saveRoutineToFile();
-    
-   
+
     void checkClassReminders();
     void showNextClass();
     void showUpcomingClasses();
     void displayReminders();
     void checkForImmediateClasses();
-    
-  
+
+    const std::vector<ClassSchedule>& getWeeklySchedule() const { return weeklySchedule; }
+    const std::vector<ClassReminder>& getActiveReminders() const { return activeReminders; }
+
+private:
     std::string getCurrentDay() const;
     int getCurrentTimeInMinutes() const;
     int parseTimeToMinutes(const std::string& timeStr) const;
@@ -69,10 +56,4 @@ private:
     bool isClassToday(const ClassSchedule& cls) const;
     bool isClassUpcoming(const ClassSchedule& cls, int currentTimeInMin) const;
     int getMinutesUntilClass(const ClassSchedule& cls, int currentTimeInMin) const;
-    
-   
-    void viewNotices();
-    void viewGrades();
-    void checkStudyPlanner();
-    void checkHabitTracker();
 };
