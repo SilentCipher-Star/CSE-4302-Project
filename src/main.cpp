27437f1f7341b3c47935a740651e0dfb59cebf6c
ASR <abusayeemrafi@gmail.com>
2026-01-31 12:25:42 +0600

    trial run

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,8 @@
 using namespace std;
 
 int main()
-{
-    cout << "\033[2J\033[1;1H"; // clears terminal
-    cout << "Hello, World!" << endl;
+{ 
+    cout << "Hello, Everyone!" << endl;
     App app;
     app.run();
 }
